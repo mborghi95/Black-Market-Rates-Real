944540307ded3ccb74730be3c239b594aeaa9be6
mborghi95 <45240291+mborghi95@users.noreply.github.com>
2025-06-23 09:17:03 -0400

    Fix Vercel deployment: Move static files to root and update server config

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ function setCachedData(key, data) {
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('.'));
 
 // Routes
 app.get('/', (req, res) => {
